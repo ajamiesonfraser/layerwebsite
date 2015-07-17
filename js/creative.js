9a62be08f1b4e604d9631166656835f587fd0cc6
A.J. Fraser <ajamiesonfraser@gmail.com>
2015-07-17 15:04:52 -0300

    beta sign up and other things

--- js/creative.js
+++ js/creative.js
@@ -46,3 +46,44 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+jQuery(function($)
+{
+    $("#contact_form").submit(function()
+    {
+        var email = $("#email").val(); // get email field value
+        var name = $("#name").val(); // get name field value
+        $.ajax(
+            {
+                type: "POST",
+                url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                data: {
+                    'key': 'Qh2mySnK5B3aA7Ypl8dcrw',
+                    'message': {
+                        'from_email': email,
+                        'from_name': name,
+                        'headers': {
+                            'Reply-To': email
+                        },
+                        'subject': 'Website Contact Form Submission',
+                        'text': 'User has requested access to Layers Beta',
+                        'to': [
+                            {
+                                'email': 'ajamiesonfraser@gmail.com',
+                                'name': 'A.J. Fraser',
+                                'type': 'to'
+                            }]
+                    }
+                }
+            })
+            .done(function(response) {
+                alert('Your message has been sent. Thank you!'); // show success message
+                $("#name").val(''); // reset field after successful submission
+                $("#email").val(''); // reset field after successful submission
+            })
+            .fail(function(response) {
+                alert('Error sending message.');
+            });
+        return false; // prevent page refresh
+    });
+});
